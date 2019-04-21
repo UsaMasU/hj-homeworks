fff48b065a4e42c4598fd68a5a0fb726dca370a6
UsaMasU <44852266+UsaMasU@users.noreply.github.com>
2019-04-21 18:58:02 +0300

    hw 1

---
+++ browser/gallery/index.js
@@ -0,0 +1,27 @@
+﻿'use strict';
+
+const img = document.getElementById('currentPhoto'); 
+const pics = ["breuer-building.jpg", "guggenheim-museum.jpg", "headquarters.jpg", "IAC.jpg", "new-museum.jpg"];
+let picNum = 0;
+
+function showPics(picNum) {
+  img.src = "i/" + pics[picNum];
+}
+
+function handleNextClick() {
+  picNum += 1;
+  if(picNum > pics.length - 1) picNum = 0;
+  showPics(picNum);
+}
+
+function handlePrevClick() {
+   picNum -= 1;
+   if(picNum < 0) picNum = 4;
+   showPics(picNum);
+}
+
+const buttonNext = document.getElementById('nextPhoto');
+buttonNext.onclick = handleNextClick;
+
+const buttonPrev = document.getElementById('prevPhoto');
+buttonPrev.onclick = handlePrevClick;
---
+++ browser/slider/index.js
@@ -0,0 +1,11 @@
+﻿'use strict';
+
+const img = document.getElementById('slider'); 
+const pics = ["airmax.png", "airmax-jump.png", "airmax-on-foot.png", "airmax-playground.png", "airmax-top-view.png"];
+let picNum = 1;
+
+function showPics() {
+  img.src = "i/" + pics[picNum];
+  picNum += 1; 
+  if(picNum > pics.length - 1) picNum = 0;
+} setInterval(showPics, 5000);
