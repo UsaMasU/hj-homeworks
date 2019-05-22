bc85a365c958bdcf54a20a3ca46098e9759a5b21
UsaMasU <44852266+UsaMasU@users.noreply.github.com>
2019-05-22 22:15:46 +0300

    update

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,6 +1,12 @@
 'use strict';
 function btnClick(event) {
-  if(event.target.classList.contains('add-to-cart')) addToCart({'title': event.target.getAttribute('data-title'), 'price': event.target.getAttribute('data-price')});
+  //if(event.target.classList.contains('add-to-cart')) addToCart({'title': event.target.getAttribute('data-title'), 'price': event.target.getAttribute('data-price')});
+  event.preventDefault();  
+  if(event.target.classList.contains('add-to-cart')) 
+    addToCart({
+       'title': event.target.getAttribute('data-title'), 
+       'price': event.target.getAttribute('data-price')
+    });
 }
 
 document.addEventListener('click', btnClick);
