4fa3000f67a9484951e1c887b6e3c5991d8f3977
UsaMasU <44852266+UsaMasU@users.noreply.github.com>
2019-05-31 14:16:24 +0300

    hw canvas

--- canvas/background/js/background.js
+++ canvas/background/js/background.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const FPS = 20;
+
 let canvas = document.querySelector('#wall');
 canvas.setAttribute('width', window.innerWidth);
 canvas.setAttribute('height', window.innerHeight);
@@ -10,10 +12,10 @@ let crosses = [];
    
 let circlesQuant = 0;
 let crossesQuant = 0; 
-let objects;
+let objectsQuant = 0;
 
-class ScreenObj {
-  constructor(x = 0, y = 0, size = 0, type = '', color = 'white', moveType = 'wave_1', rotateAngle = 0, rotateSpeed = 0.2) {
+class FlyObj {
+  constructor(x = 0, y = 0, size = 0, type, color = 'white', moveType = 'wave_1', rotateAngle = 0, rotateSpeed = 0.2) {
     this.x = x;
     this.y = y;
     this.baseX = x;
@@ -28,23 +30,23 @@ class ScreenObj {
     this.rotateAngle = rotateAngle;
   }
   
-  nextPoint_1(time) {
+  moveNextPoint_1(time) {
     this.x =  this.baseX + Math.sin((50 + this.baseX + (time / 10)) / 100) * 3;
     this.y = this.baseY + Math.sin((45 + this.baseX + (time / 10)) / 100) * 4;
   }
   
-  nextPoint_2(time) {
+  moveNextPoint_2(time) {
     this.x = this.baseX + Math.sin((this.baseX + (time / 10)) / 100) * 5;
     this.y = this.baseY + Math.sin((10 + this.baseX + (time / 10)) / 100) * 2;
   }
   
-  move(time) {
+  show(time) {
     switch(this.moveType) {
       case 'wave_1':
-        this.nextPoint_1(time);
+        this.moveNextPoint_1(time);
         break;
       case 'wave_2':
-        this.nextPoint_2(time);
+        this.moveNextPoint_2(time);
         break;
       default:
     }
@@ -59,29 +61,26 @@ class ScreenObj {
       case 'circle': 
         ctx.arc(this.x, this.y, this.circleSize, 0, (Math.PI/180) * 360);
         break;
-   
-   case 'cross':
-        //this.rotateAngle += this.rotateSpeed;
-        //if(this.rotateAngle > 360) this.rotateAngle = 0;
-        //if(this.rotateAngle < 0) this.rotateAngle = 360;
-        //ctx.translate(this.x, this.y);
-        ctx.rotate(this.rotateAngle * Math.PI/180); 
-              
-        ctx.moveTo(this.x, this.y + this.crossSize);
-        ctx.lineTo(this.x, this.y + this.crossSize);
-        ctx.moveTo(this.x, this.y - this.crossSize);
-        ctx.lineTo(this.x, this.y + this.crossSize);
-        ctx.moveTo(this.x + this.crossSize, this.y);
-        ctx.lineTo(this.x + this.crossSize, this.y);
-        ctx.moveTo(this.x - this.crossSize, this.y);
-        ctx.lineTo(this.x + this.crossSize, this.y);
+      case 'cross':
+        ctx.save();
+          ctx.translate(this.x, this.y);
+          this.rotateAngle += this.rotateSpeed;   
+          ctx.rotate(this.rotateAngle * Math.PI/180); 
+          
+          ctx.moveTo(-this.crossSize, 0);
+          ctx.lineTo(this.crossSize, 0);
+          ctx.moveTo(0, 0);
+          ctx.lineTo(0, this.crossSize);
+          ctx.moveTo(0, -this.crossSize);
+          ctx.lineTo(0, 0); 
+          
+        ctx.restore()
         break;
       default:
     }
     ctx.stroke();
     ctx.closePath();
   }
-  
 }
 
 
@@ -89,14 +88,13 @@ function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-
 function randomFloat(min, max) {
   return Math.random() * (max - min) + min;
 }
   
-objects = randomInt(50, 200);
-if(objects % 2 == 0) circlesQuant = objects / 2;
-else circlesQuant = (objects + 1) / 2;
+objectsQuant = randomInt(50, 200);
+if(objectsQuant % 2 == 0) circlesQuant = objectsQuant / 2;
+else circlesQuant = (objectsQuant + 1) / 2;
 crossesQuant = circlesQuant;
     
 for(let circle = 0; circle < circlesQuant; circle ++) {
@@ -105,7 +103,7 @@ for(let circle = 0; circle < circlesQuant; circle ++) {
   let size = randomFloat(0.1, 0.6);
   let moveType = '';
   randomInt(1, 2) == 1 ? moveType = 'wave_1': moveType = 'wave_2';
-  circles.push(new ScreenObj(posX, posY, size, 'circle', 'white', moveType));
+  circles.push(new FlyObj(posX, posY, size, 'circle', 'white', moveType));
 }
     
 for(let cross = 0; cross < crossesQuant; cross ++) {
@@ -116,25 +114,19 @@ for(let cross = 0; cross < crossesQuant; cross ++) {
   let angleSpeed = randomFloat(-0.2, 0.2);
   let moveType = '';
   randomInt(1, 2) == 1 ? moveType = 'wave_1': moveType = 'wave_2';
-  crosses.push(new ScreenObj(posX, posY, size, 'cross', 'white', moveType, angle , angleSpeed));
+  crosses.push(new FlyObj(posX, posY, size, 'cross', 'white', moveType, angle, angleSpeed));
 }
   
-function draw() {  
-     canvas.width = window.innerWidth;
-     canvas.height = window.innerHeight;
-
-     for(let circ of circles) {
-       circ.move(Date.now());
-     }
-     
-     for(let cross of crosses) {
-       cross.move(Date.now());
-     }
+function drawFrame() {  
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  
+  for(let circ of circles) circ.show(Date.now());
+  for(let cross of crosses) cross.show(Date.now());
 }
        
-const FPS = 20;
 function syncFrame() {
-  draw();
+  drawFrame();
   setTimeout(function() {         
     requestAnimationFrame(syncFrame);
   }, 1000 / FPS);
