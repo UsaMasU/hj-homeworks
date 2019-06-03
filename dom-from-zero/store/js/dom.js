207e35555a0ded2dce0f0cc19c00a7416ae26e61
UsaMasU <44852266+UsaMasU@users.noreply.github.com>
2019-06-04 02:08:58 +0300

    hw dom-from-zero

--- dom-from-zero/store/js/dom.js
+++ dom-from-zero/store/js/dom.js
@@ -1,37 +1,18 @@
 'use strict';
 
-function el(tagName, attributes, children) {
-  const element = document.createElement(tagName);
-  if (typeof attributes === 'object') {
-    Object.keys(attributes).forEach(i => element.setAttribute(i, attributes[i]));
-  }
-  if (typeof children === 'string') {
-    element.textContent = children;
-  } else if (children instanceof Array) {
-    children.forEach(child => element.appendChild(child));
-  }
-  return element;
-}
-
 function createElement(content) {
-  //console.log(content);
-  let dd = e('h2', null, item.brand, item);
-  console.log(dd);
   const element = document.createElement(content.name);
-  //console.log(content.props);
-
-  if (typeof content.props === 'object') {
-    Object.keys(content.props).forEach(i => element.setAttribute(i, content.props[i]));
+  if(content.props != null) {
+    if(typeof content.props === 'object') {    
+      for(let atr in content.props) {
+        element.setAttribute(atr, content.props[atr]);
+      }
+    }
   }
-  
+  if(typeof content.childs[0] === 'string') {
+     element.textContent = content.childs[0];
+  }else if(content.childs instanceof Array) {
+     content.childs.forEach(child => element.appendChild(createElement(child)));
+  }
   return element;
 }
-
-//const elems = content.map(createElement);
-
-//const node = elems.reduce((fragment, currentValue) => {
-//    node.appendChild(currentValue);
-//    return node;
-//  }, document.createDocumentFragment());
-  
-//  wrapper.appendChild(node);
